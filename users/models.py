7b17622f9cb4c5e85af9b6b556e9524026191f33
Lucas <lucasspindolafernandes196@hotmail.com>
2023-02-23 16:52:22 -0300

    serializer-refactoring

--- users/models.py
+++ users/models.py
@@ -5,3 +5,5 @@ from django.contrib.auth.models import AbstractUser
 class User(AbstractUser):
     first_name = models.CharField(max_length=50)
     last_name = models.CharField(max_length=50)
+    username = models.CharField(max_length=150, unique=True, error_messages={"unique": "A user with that username already exists."})
+    email = models.EmailField(max_length=127, unique=True, error_messages={"unique": "This field must be unique."})
